7968dc0ef4f05ff1f6580118cda7718a7f90aef9
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2020-02-05 21:18:24 +0100

    additional DB migrations

---
+++ VidlyApp/Models/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlyApp.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+        public short SignUpfee { get; set; }
+        public byte DurationinMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
